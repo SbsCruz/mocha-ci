5d3eb59e3ad1903c65d3373647a23193748bf887
Sebas Cruz <sebas.cruz750@gmail.com>
2024-12-26 11:28:10 -0500

    add converter.js and server.js

---
+++ app/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { rgbToHex, hexToRgb } from "./converter.js";
+
+var app = express();
+
+const PORT = 3000;
+
+app.get("/rgbToHex", function (req, res) {
+  var red = parseInt(req.query.red, 10);
+  var green = parseInt(req.query.green, 10);
+  var blue = parseInt(req.query.blue, 10);
+  var hex = rgbToHex(red, green, blue);
+  res.send(hex);
+});
+
+app.get("/hexToRgb", function (req, res) {
+  var hex = req.query.hex;
+  var rgb = hexToRgb(hex);
+  res.send(JSON.stringify(rgb));
+});
+
+app.listen(PORT);
+
+console.log("app listening in port: ", PORT)
